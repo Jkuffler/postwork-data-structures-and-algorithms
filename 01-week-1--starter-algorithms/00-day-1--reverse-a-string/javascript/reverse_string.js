102667bc8af47f60fcc1648237392dd3ea81aef8
Jason <jakuffler@gmail.com>
2022-02-01 05:51:43 -0500

    refreshing the algo muscles

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,6 +1,10 @@
 function reverseString(str) {
-  // type your code here
-}
+  let solution = ""
+    for (i = str.length - 1; i > -1; --i) {
+      solution = solution + str[i]
+    }
+    return solution
+};
 
 if (require.main === module) {
   // add your own tests in here
@@ -16,4 +20,12 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
-// And a written explanation of your solution
+  /*
+  let answer be an empty str
+  pass input string into a loop
+  beginning from the end of str add each element to the one before it into the answer var
+  */
+
+  // And a written explanation of your solution
+
+/* in order to achieve the reverse string i've passed the str into a loop in order to transpose the final element of input into the beginning of a new str var named solution*/
